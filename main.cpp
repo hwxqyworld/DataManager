fa2eff17c15437946380bd4c44e67f73a2d3ee31
XQYWorld <xqyworld@wpgzs.top>
2026-01-11 22:10:50 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -180,21 +180,25 @@ static int raidfs_truncate(const char *path, off_t size,
 // ------------------------------------------------------------
 // FUSE ops
 // ------------------------------------------------------------
-static const struct fuse_operations raidfs_ops = {};
-raidfs_ops.getattr    = raidfs_getattr,
-raidfs_ops.readdir    = raidfs_readdir,
-raidfs_ops.create     = raidfs_create,
-raidfs_ops.unlink     = raidfs_unlink,
-raidfs_ops.truncate   = raidfs_truncate,
-raidfs_ops.open       = raidfs_open,
-raidfs_ops.read       = raidfs_read,
-raidfs_ops.write      = raidfs_write,
+static struct fuse_operations raidfs_ops = {};
+static void init_ops() {
+    raidfs_ops.getattr    = raidfs_getattr,
+    raidfs_ops.readdir    = raidfs_readdir,
+    raidfs_ops.create     = raidfs_create,
+    raidfs_ops.unlink     = raidfs_unlink,
+    raidfs_ops.truncate   = raidfs_truncate,
+    raidfs_ops.open       = raidfs_open,
+    raidfs_ops.read       = raidfs_read,
+    raidfs_ops.write      = raidfs_write;
+}
 
 // ------------------------------------------------------------
 // main
 // ------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+    init_ops();
+    
     // ./cloudraidfs <mountpoint> <dir0> <dir1> <dir2> <dir3> <dir4> [FUSE options...]
 
     if (argc < 7) {
